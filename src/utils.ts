2fe532827a09ea541f9d57c116b3539541d63355
David Oti <shadow@Davids-MacBook-Pro.local>
2022-12-19 10:03:33 +0100

    Add guidance preset options

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { DiffusionSampler } from 'stability-sdk/gooseai/generation/generation_pb'
+import { DiffusionSampler, GuidancePreset } from 'stability-sdk/gooseai/generation/generation_pb'
 
 export const range = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min + 1)) + min
@@ -12,4 +12,14 @@ export const diffusionMap = {
   k_dpm_2: DiffusionSampler.SAMPLER_K_DPM_2,
   k_dpm_2_ancestral: DiffusionSampler.SAMPLER_K_DPM_2_ANCESTRAL,
   k_lms: DiffusionSampler.SAMPLER_K_LMS,
+}
+
+export const clipGuidanceMap = {
+  none: GuidancePreset.GUIDANCE_PRESET_NONE,
+  simple: GuidancePreset.GUIDANCE_PRESET_SIMPLE,
+  fast_blue: GuidancePreset.GUIDANCE_PRESET_FAST_BLUE,
+  fast_green: GuidancePreset.GUIDANCE_PRESET_FAST_GREEN,
+  slow: GuidancePreset.GUIDANCE_PRESET_SLOW,
+  slower: GuidancePreset.GUIDANCE_PRESET_SLOWER,
+  slowest: GuidancePreset.GUIDANCE_PRESET_SLOWEST,
 }
